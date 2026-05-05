fca1b90f089bb9c50c040be7046770d2fa697e6d
GoldenPotato137 <GoldenPotato137@gmail.com>
2026-05-05 22:17:26 +0800

    Initial commit

---
+++ PotatoVN.App.PluginBase/Plugin_Ui.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using GalgameManager.WinApp.Base.Models.Plugin;
+using Microsoft.UI.Xaml;
+using PotatoVN.App.PluginBase.Controls;
+
+namespace PotatoVN.App.PluginBase;
+
+public partial class Plugin
+{
+    private bool _uiInit;
+    
+    private void InitUi()
+    {
+        if (_uiInit) return;
+        _hostApi.RegisterSidebarButton(new SidebarButtonInfo
+        {
+           Id = "sidebarButton1",
+           Text = "插件按钮",
+           Placement = SidebarButtonPlacement.Menu, 
+           FluentGlyph = "&#xE709;",
+        }, () =>
+        {
+            _hostApi.NavigateTo(typeof(ExamplePage), "Example Page");
+            return Task.CompletedTask;
+        });
+        _uiInit = true;
+    }
+
+    public FrameworkElement CreateSettingUi() => new UserControl1(_data);
+}
